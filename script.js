ea659a0f61d856a707a87797e7553fa52e2b06fe
Abhishek Gandhi <83358338+abhi0706-source@users.noreply.github.com>
2024-11-15 11:06:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Initialize EmailJS with your user ID
 (function () {
-    emailjs.init('YOUR_USER_ID'); // Replace with your EmailJS user ID
+    emailjs.init('service_fpu4wvr'); // Replace with your EmailJS user ID
 })();
 
 $(document).ready(function () {
